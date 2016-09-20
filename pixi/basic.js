e389390cfe9c95cc6927c5351e9e667e9243044b
Neoco <Neoco.wlp1002@gmail.com>
2016-09-20 15:15:04 +0800

    Learning pixi.js

---
+++ pixi/basic.js
@@ -0,0 +1,59 @@
+// var stage = new PIXI.Container(),
+//   renderer = PIXI.autoDetectRenderer(256, 256);
+// document.body.appendChild(renderer.view);
+// 
+// PIXI.loader
+//   .add("img/badguy.png")
+//   .load(setup);
+// 
+// function setup() {
+//   var cat = new PIXI.Sprite( PIXI.loader.resources["img/badguy.png"].texture);
+// 
+//   stage.addChild(cat);
+// 
+//   renderer.render(stage);
+// }
+//
+// pixi.min.js
+var Container = PIXI.Container,
+  autoDetectRenderer = PIXI.autoDetectRenderer,
+  loader = PIXI.loader,
+  resources = PIXI.loader.resources,
+  Sprite = PIXI.Sprite;
+var TextureCache = PIXI.utils.TextureCache;
+
+var globleX = window.innerWidth - 2,
+  globleY = window.innerHeight - 2;
+
+var stage = new Container(),
+  renderer = autoDetectRenderer(window.innerWidth - 2, window.innerHeight - 2);
+document.body.appendChild(renderer.view);
+
+loader
+  .add("img/badguy.png")
+  .add("img/tileset.png")
+  .on("progress", loadProgressHandler)
+  .load(setup);
+
+function loadProgressHandler(loader, resource) {
+  console.log("loading:" + resource.url);
+  console.log("Progress:" + loader.progress + "%");
+}
+
+function setup(){
+  var cat = new Sprite(resources["img/badguy.png"].texture);
+  cat.position.set(globleX / 2, globleY /2);
+  cat.pivot.set(100, 100);
+  cat.rotation = 0;
+  stage.addChild(cat);
+
+  var texture = TextureCache["img/tileset.png"];
+  var rectangle = new PIXI.Rectangle(96, 64,32, 32);
+  texture.frame = rectangle;
+  var rocket = new Sprite(texture);
+  rocket.position.set(100, 100);
+  stage.addChild(rocket);
+
+  renderer.render(stage);
+  console.log("setup");
+}
